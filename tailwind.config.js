f1e65259f8d2d04afe5a753ce6368152172450be
Yousuf Mohammad Amin <42436908+y-m-amin@users.noreply.github.com>
2025-08-29 21:35:47 +0600

    some style changes basic responsiveness

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ['"Hind Madurai"', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+};
